62aae6ee279d1a05bc322b13d8c0e9e14685a8f7
Dennis Bruijn <dbbruijn@gmail.com>
2016-09-30 23:20:00 +0200

    Added styles and mock data

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 
 import { HomePage } from '../home/home';
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';ß
+import { ContactPage } from '../contact/contact';
 import { MovieListPage } from '../movie-list/movie-list';
 import {TimelinePage} from '../timeline/timeline';
 
