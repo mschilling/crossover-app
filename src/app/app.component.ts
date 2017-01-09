db8863b93499096921f6a996fbb6de34c086b833
Michael Schilling <mschilling@arkid.nl>
2017-01-09 16:33:48 +0100

    Move Timeline page implementation from HomePage to (newly generated) TimelinePage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
-
+import { TimelinePage } from '../pages/timeline/timeline';
 
 @Component({
   templateUrl: 'app.html'
---
+++ src/pages/timeline/timeline.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+/*
+  Generated class for the Timeline page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-timeline',
+  templateUrl: 'timeline.html'
+})
+export class TimelinePage {
+  cards: FirebaseListObservable<any>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, af: AngularFire) {
+    console.log('Fetch data from Firebase');
+    this.cards = af.database.list('/timelines/marvel', {
+      query: {
+        orderByChild: 'aired',
+        limitToLast: 150
+      }
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TimelinePage');
+  }
+
+}
