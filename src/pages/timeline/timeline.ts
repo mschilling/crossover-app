c4432cf5f0fea9ad09e94b9ff510270f41a33baf
Michael Schilling <mschilling@arkid.nl>
2017-01-10 07:59:12 +0100

    Set timeline pagesize to 500

--- src/pages/timeline/timeline.ts
+++ src/pages/timeline/timeline.ts
@@ -34,7 +34,7 @@ export class TimelinePage {
     this.cards = af.database.list(path, {
       query: {
         orderByChild: 'aired',
-        limitToLast: 15
+        limitToLast: 500
       }
     });
   }
