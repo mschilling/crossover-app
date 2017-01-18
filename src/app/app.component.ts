66c419bcf5015d7308dff997a6874ff12377615f
Dennis Bruijn <dbbruijn@gmail.com>
2017-01-18 19:19:39 +0100

    Updated firebase dependecies, removed unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
-import { TimelinePage } from '../pages/timeline/timeline';
 
 @Component({
   templateUrl: 'app.html'
